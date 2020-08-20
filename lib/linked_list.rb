3cbac36b958eaac989d355694b0e971b76d829db
Chris M <chris@adadevelopersacademy.org>
2020-08-20 11:57:15 -0700

    Update linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -18,12 +18,16 @@ class LinkedList
 
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
+    # Time Complexity: ?
+    # Space Complexity: ?
     def add_first(value)
       raise NotImplementedError
     end
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
+    # Time Complexity: ?
+    # Space Complexity: ?
     def search(value)
       raise NotImplementedError
     end
@@ -36,12 +40,16 @@ class LinkedList
 
     # method to return the min value in the linked list
     # returns the data value and not the node
+    # Time Complexity: ?
+    # Space Complexity: ?
     def find_min
       raise NotImplementedError
     end
 
 
     # method that returns the length of the singly linked list
+    # Time Complexity: ?
+    # Space Complexity: ?
     def length
       raise NotImplementedError
     end
@@ -49,22 +57,30 @@ class LinkedList
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns nil if there are fewer nodes in the linked list than the index value
+    # Time Complexity: ?
+    # Space Complexity: ?
     def get_at_index(index)
       raise NotImplementedError
     end
 
     # method to print all the values in the linked list
+    # Time Complexity: ?
+    # Space Complexity: ?
     def visit
       raise NotImplementedError
     end
 
     # method to delete the first node found with specified value
+    # Time Complexity: ?
+    # Space Complexity: ?
     def delete(value)
       raise NotImplementedError
     end
 
     # method to reverse the singly linked list
     # note: the nodes should be moved and not just the values in the nodes
+    # Time Complexity: ?
+    # Space Complexity: ?
     def reverse
       raise NotImplementedError
     end
@@ -72,12 +88,16 @@ class LinkedList
 
     ## Advanced Exercises
     # returns the value at the middle element in the singly linked list
+    # Time Complexity: ?
+    # Space Complexity: ?
     def find_middle_value
       raise NotImplementedError
     end
 
     # find the nth node from the end and return its value
     # assume indexing starts at 0 while counting to n
+    # Time Complexity: ?
+    # Space Complexity: ?
     def find_nth_from_end(n)
       raise NotImplementedError
     end
@@ -85,6 +105,8 @@ class LinkedList
     # checks if the linked list has a cycle. A cycle exists if any node in the
     # linked list links to a node already visited.
     # returns true if a cycle is found, false otherwise.
+    # Time Complexity: ?
+    # Space Complexity: ?
     def has_cycle
       raise NotImplementedError
     end
@@ -93,23 +115,31 @@ class LinkedList
     # Additional Exercises 
     # returns the value in the first node
     # returns nil if the list is empty
+    # Time Complexity: ?
+    # Space Complexity: ?
     def get_first
       raise NotImplementedError
     end
 
     # method that inserts a given value as a new last node in the linked list
+    # Time Complexity: ?
+    # Space Complexity: ?
     def add_last(value)
       raise NotImplementedError
     end
 
     # method that returns the value of the last node in the linked list
     # returns nil if the linked list is empty
+    # Time Complexity: ?
+    # Space Complexity: ?
     def get_last
       raise NotImplementedError
     end
 
     # method to insert a new node with specific data value, assuming the linked
     # list is sorted in ascending order
+    # Time Complexity: ?
+    # Space Complexity: ?
     def insert_ascending(value)
       raise NotImplementedError
     end
