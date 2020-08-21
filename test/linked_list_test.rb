8148fb86b3f32dd0ab7b84b8069482f11373529f
CheezItMan <chris@adadevelopersacademy.org>
2020-08-20 18:34:07 -0700

    modify tests

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -186,7 +186,7 @@ describe LinkedList do
             expect(@list.find_max).must_equal 9
             expect(@list.find_min).must_equal 3
 
-            # delete fist node (requires updating head)
+            # delete middle node (requires updating head)
             @list.delete(4)
             expect(@list.get_first).must_equal 3
             expect(@list.length).must_equal 2
