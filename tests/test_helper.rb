c79d6a5611e79035186987643a101253837d46a1
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-02 15:12:26 -0700

    remove ruby test helper

--- tests/test_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/reporters'
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-
-require_relative '../lib/linked_list'
-
